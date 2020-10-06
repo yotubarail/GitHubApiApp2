3a432f003648d74073ffe09ecffac7ad9b20a58f
Shota Takinami <shota_t@i.softbank.jp>
2020-10-06 15:25:16 +0900

    Initial Commit

---
+++ GitHubApiApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubApiApp2
+//
+//  Created by 滝浪翔太 on 2020/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
