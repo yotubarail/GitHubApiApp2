d196c5435324f379ca321db69803c411d9a88bc9
Shota Takinami <shota_t@i.softbank.jp>
2020-10-28 10:08:36 +0900

    SDWebImageを用いてプロフィール画像をキャッシュさせる

--- GitHubApiApp2/Views/TopViewController.swift
+++ GitHubApiApp2/Views/TopViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class TopViewController: UIViewController {
 
@@ -75,7 +76,7 @@ extension TopViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SearchUserTableViewCell
         let userViewData = userData[indexPath.row]
         cell.userNameLabel.text = userViewData.login
-        cell.avatarImageView.image = UIImage(url: userViewData.avatarUrl)
+        cell.avatarImageView.sd_setImage(with: URL(string: userViewData.avatarUrl), completed: nil)
         cell.userTypeLabel.text = userViewData.type
         
         return cell
